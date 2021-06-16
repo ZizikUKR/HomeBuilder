92ae0bd7dae28196b3a339c9cdb27a596c2024af
Mykola Zinenko <mzine@softserveinc.com>
2021-06-16 22:43:49 +0300

    added GetAllFoodWasteView

---
+++ Homebuilder.Domain/Views/Guids/Foods/GetAllFoodWasteView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Homebuilder.Domain.Views.Guids.Foods
+{
+    public class GetAllFoodWasteView
+    {
+        public List<FoodWasteGetAllViewItem> Items { get; set; } = new List<FoodWasteGetAllViewItem>();
+    }
+
+    public class FoodWasteGetAllViewItem
+    {
+        public string Id { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryId { get; set; }
+        public FoodCategoryView Category { get; set; }
+    }
+}
